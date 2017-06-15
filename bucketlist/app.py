663553e6b6362bc6bd369e33fcb263640bccb8cf
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-06-15 12:26:56 +0300

    [ft #147186931] Use create_app method to create app with saved configurations

--- bucketlist/app.py
+++ bucketlist/app.py
@@ -1,13 +1,18 @@
 """Module to initialise and configure the flask app and the db."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from configuration.config import app_config
 
-# initialise flask app
-app = Flask(__name__)
 
-# configure and create a db object
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
-                                                    basedir, 'bucketlist.db')
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+
+
+def create_app(configuration):
+    """Initialise and cofigure the app and db."""
+    app = Flask(__name__)
+    app.config.from_object(app_config[configuration])
+    db.init_app(app)
+    return app
+
+
+app = create_app("development")
