98debd024b4b85dba0da676b80e11c5fc53d7a11
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-06-15 10:22:04 +0300

    [ft #147182823] Include code for configuration and setup of a database

--- bucketlist/app.py
+++ bucketlist/app.py
@@ -1,4 +1,13 @@
-"""Module to initialise and configure the flask app."""
+"""Module to initialise and configure the flask app and the db."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+# initialise flask app
 app = Flask(__name__)
+
+# configure and create a db object
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+                                                    basedir, 'bucketlist.db')
+db = SQLAlchemy(app)
