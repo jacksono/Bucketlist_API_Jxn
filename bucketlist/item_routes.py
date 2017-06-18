537588f8e2f6e0e61e03dad9114e216de828e68b
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-06-18 21:45:55 +0300

    [ft #147193983] Add controller for item routes

---
+++ bucketlist/item_routes.py
@@ -0,0 +1,26 @@
+"""Module to define items endpoints."""
+
+from flask_restful import Resource, reqparse
+from bucketlist.models import Item
+from bucketlist.helper_functions import add_item
+from flask import g
+
+
+class CreateItem(Resource):
+    """Create a new bucketlist item. Route: /bucketlists/<id>/items/."""
+
+    def post(self, id):
+        """"Create a new bucketlist item."""
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "name",
+            required=True,
+            help="Please enter a bucketlist item name.")
+        parser.add_argument(
+                            "done",
+                            required=True,
+                            help="Please enter the status")
+        args = parser.parse_args()
+        name, done = args["name"], args["done"]
+        item = Item(name=name, done=done, bucketlist_id=g.bucketlist.id)
+        return add_item(item)
