25695482325cefa68373372534ad639ecb26db68
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-06-19 18:54:08 +0300

    [ft #147194059] Add controller logic for delete item route

--- bucketlist/item_routes.py
+++ bucketlist/item_routes.py
@@ -3,6 +3,7 @@
 from flask_restful import Resource, reqparse
 from bucketlist.models import Item
 from bucketlist.helper_functions import add_item
+from bucketlist.app import db
 
 
 class CreateItem(Resource):
@@ -23,3 +24,22 @@ class CreateItem(Resource):
         name, done = args["name"], args["done"]
         item = Item(name=name, done=done, bucketlist_id=id)
         return add_item(item)
+
+
+class DeleteItem(Resource):
+    """Delete bucketlist item. Route: /bucketlists/<id>/items/<item_id>."""
+
+    def delete(self, id, item_id):
+        """Delete bucketlist item."""
+        items = Item.query.filter_by(bucketlist_id=id).all()
+        if items:
+            if int(item_id) <= len(items):
+                item = items[int(item_id) - 1]
+                db.session.delete(item)
+                db.session.commit()
+                return {"message": "Item deleted succesfully"}
+            else:
+                return {"message": "That item doesnot exist"}
+        else:
+            return {"message": "ERROR!: That Bucketlist does not exists"
+                    " or does not have any items"}
