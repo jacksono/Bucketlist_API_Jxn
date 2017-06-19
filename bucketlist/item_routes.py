8901657c1df64a14c38d6a7e58bdfecae71b0f9b
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-06-19 15:57:22 +0300

    [ft #147194017] Add logic to pick correct item to be updated

--- bucketlist/item_routes.py
+++ bucketlist/item_routes.py
@@ -3,6 +3,9 @@
 from flask_restful import Resource, reqparse
 from bucketlist.models import Item
 from bucketlist.helper_functions import add_item
+from datetime import datetime
+from bucketlist.app import db
+from sqlalchemy.exc import IntegrityError
 
 
 class CreateItem(Resource):
@@ -23,3 +26,41 @@ class CreateItem(Resource):
         name, done = args["name"], args["done"]
         item = Item(name=name, done=done, bucketlist_id=id)
         return add_item(item)
+
+
+class UpdateItem(Resource):
+    """Update a bucketlist item.Route: /bucketlists/<id>/items/<item_id>."""
+
+    def put(self, id, item_id):
+        """Update a bucketlist item."""
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "name",
+            required=True,
+            help="Please enter new item name.")
+        parser.add_argument(
+                            "done",
+                            required=True,
+                            help="Please enter item status")
+        args = parser.parse_args()
+        name, done = args["name"], args["done"]
+        items = Item.query.filter_by(bucketlist_id=id).all()
+        if items:
+            if int(item_id) <= len(items):
+                item_to_update = items[int(item_id) - 1]
+                item_to_update.name = name
+                item_to_update.done = done
+                item_to_update.date_modified = datetime.now()
+                db.session.add(item_to_update)
+                try:
+                    db.session.commit()
+                except IntegrityError:
+                    """Show when the item already exists"""
+                    db.session.rollback()
+                    return {"message": "Error: " + item_to_update.name +
+                            " already exists."}
+                return {"message": "Item updated succesfully"}
+            else:
+                return {"message": "Item doesnot exist in the bucketlist"}
+        else:
+            return {"message": "That bucketlist has no items"}
