c55524517d5d6f45addce47a09a6c4a6d2783d78
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-06-20 14:43:10 +0300

    Modify tests to use token authentication

--- tests/test_items.py
+++ tests/test_items.py
@@ -7,12 +7,22 @@ from bucketlist.models import Item
 class TestItem(BaseTest):
     """Test items routes."""
 
+    def get_token(self):
+        """Return authentication token."""
+        self.user = {"username": "user",
+                     "password": "password"}
+        r = self.app.post("/api/v1/auth/login/",
+                          data=self.user)
+        output = json.loads(r.data.decode())
+        token = output.get("token")
+        return {"token": token}
+
     def test_can_add_new_item(self):
         """Tests if a user can add a new item in a bucketlist."""
         self.item = {"name": "Go to Hawaii", "done": "True",
                      "buckeltist_id": 1}
         r = self.app.post("/api/v1/bucketlists/1/items/", data=self.item,
-                          headers={"username": "user", "bucketlist": "Travel"})
+                          headers=self.get_token())
         self.assertEqual(r.status_code, 201)
         message = json.loads(r.data.decode())
         self.assertIn("created successfully", message["message"])
@@ -22,14 +32,15 @@ class TestItem(BaseTest):
         self.item = {"name": "Enjoy the beautiful beaches of Hawaii",
                      "done": "True", "buckeltist_id": 1}
         r = self.app.post("/api/v1/bucketlists/1/items/", data=self.item,
-                          headers={"username": "user"})
+                          headers=self.get_token())
         message = json.loads(r.data.decode())
         self.assertIn("already exists", message["message"])
 
     def test_user_can_update_a_bucketlist_item(self):
         """Tests that a user can update an existing bucketlist item."""
         self.item = {"name": "New Item", "bucketlist_id": 1, "done": "True"}
-        r = self.app.put("/api/v1/bucketlists/1/items/1", data=self.item)
+        r = self.app.put("/api/v1/bucketlists/1/items/1", data=self.item,
+                         headers=self.get_token())
         self.assertEqual(r.status_code, 200)
         self.assertTrue(Item.query.filter_by(name="New Item").first())
         self.assertFalse(Item.query.filter_by(
@@ -40,10 +51,11 @@ class TestItem(BaseTest):
         self.item = {"name": "Go to Hawaii", "done": "True",
                      "buckeltist_id": 1}
         self.app.post("/api/v1/bucketlists/1/items/", data=self.item,
-                      headers={"username": "user", "bucketlist": "Travel"})
+                      headers=self.get_token())
         self.item2 = {"name": "Enjoy the beautiful beaches of Hawaii",
                       "bucketlist_id": 1, "done": "True"}
-        r = self.app.put("/api/v1/bucketlists/1/items/2", data=self.item2)
+        r = self.app.put("/api/v1/bucketlists/1/items/2", data=self.item2,
+                         headers=self.get_token())
         message = json.loads(r.data.decode())
         self.assertIn("already exists", message["message"])
 
@@ -51,7 +63,8 @@ class TestItem(BaseTest):
         """Tests for an error message shown when a user tries to update an item which doesnot exist.""" # noqa
         self.item = {"name": "Enjoy the beautiful beaches of Hawaii",
                      "bucketlist_id": 1, "done": "True"}
-        r = self.app.put("/api/v1/bucketlists/1/items/2", data=self.item)
+        r = self.app.put("/api/v1/bucketlists/1/items/2", data=self.item,
+                         headers=self.get_token())
         message = json.loads(r.data.decode())
         self.assertIn("doesnot exist", message["message"])
 
@@ -61,16 +74,18 @@ class TestItem(BaseTest):
                            "description": "I want to marry a princcess",
                            "created_by": 1}
         self.app.post("/api/v1/bucketlists/", data=self.bucketlist,
-                      headers={"username": "user"})
+                      headers=self.get_token())
         self.item = {"name": "Enjoy the beautiful beaches of Hawaii",
                      "bucketlist_id": 1, "done": "True"}
-        r = self.app.put("/api/v1/bucketlists/2/items/1", data=self.item)
+        r = self.app.put("/api/v1/bucketlists/2/items/1", data=self.item,
+                         headers=self.get_token())
         message = json.loads(r.data.decode())
         self.assertIn("has no items", message["message"])
 
     def test_user_can_delete_a_bucketlist_item(self):
         """Tests that a user can delete an existing bucketlist item."""
-        r = self.app.delete("/api/v1/bucketlists/1/items/1")
+        r = self.app.delete("/api/v1/bucketlists/1/items/1",
+                            headers=self.get_token())
         message = json.loads(r.data.decode())
         self.assertIn("deleted succesfully", message["message"])
         self.assertFalse(Item.query.all())
